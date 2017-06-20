50ea11154f0324397db45a044c12b21f7dd0cc35
nishantshri <nishant.shrivastava11@gmail.com>
2017-06-21 00:31:08 +1000

    Update CalcNumberSequence.cs

--- NumericSequenceWorldNomads/NumericSequenceWorldNomads/Services/CalcNumberSequence.cs
+++ NumericSequenceWorldNomads/NumericSequenceWorldNomads/Services/CalcNumberSequence.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,48 +11,53 @@ namespace NumericSequenceWorldNomads.Services
     public class CalcNumberSequence : ICalcNumberSeq
     {
         
-        #region ICalcNumberSeq Members
+        #region Public Members
 
         public string GetAllNumbers(int number)
         {
-            return GetNumbers(Convert.ToInt32(EnumValues.StartNumber), number, i => i.ToString(), i => ++i);
+            return GetNumbers(Convert.ToInt32(EnumValues.StartNumber), number, i => ++i);
         }
 
         public string GetAllOddNumbers(int number)
         {
-            return GetNumbers(Convert.ToInt32(EnumValues.StartNumber), number, i => i.ToString(), i => i+2);
+            return GetNumbers(Convert.ToInt32(EnumValues.StartNumber), number, i => i+2);
         }
 
         public string GetAllEvenNumbers(int number)
         {
-            return GetNumbers(Convert.ToInt32(EnumValues.StartEvenNumber), number, i => i.ToString(), i => i + 2);
+            return GetNumbers(Convert.ToInt32(EnumValues.StartEvenNumber), number, i => i + 2);
         }
 
         public string GetFibonacciSeries(int number)
         {
             int current = 0;
             int next = 1;
-            return GetNumbers(0, number, i => i.ToString(), i => { int temp = next; next = current + next; current = temp; return current; });            
+            return GetNumbers(0, number, i => { int temp = next; next = current + next; current = temp; return current; });            
         }
 
         public string GetConditionNumber(int number)
         {
-            return GetNumbers(Convert.ToInt32(EnumValues.StartNumber), number, i => i % 15 == 0 ? "Z" : i % 3 == 0 ? "C" : i % 5 == 0 ? "E" : i.ToString(), i => ++i);
+            return GetNumbers(Convert.ToInt32(EnumValues.StartNumber),number , i => ++i,true);
         }
 
         #endregion
 
-        private static string GetNumbers(int startNum, int number, Func<int, string> convert, Func<int, int> increment)
+        private static string GetNumbers(int startNum, int number, Func<int, int> increment,bool? isSubstitute = false)
         {
             var sb = new StringBuilder();
-            
+            string result = string.Empty;
+           
             try
             {
                while (startNum <= number)
                 {
                     if (sb.Length > 0)
                         sb.Append(",");
-                    sb.Append(convert(startNum));
+                    if (isSubstitute.Value)
+                        result = startNum % 15 == 0 ? "Z" : startNum % 3 == 0 ? "C" : startNum % 5 == 0 ? "E" : startNum.ToString();
+                    else
+                        result = startNum.ToString();
+                    sb.Append(result);
                     startNum = increment(startNum);
                 }
                 return sb.ToString();
